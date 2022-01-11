b210218a3774c24f16c26349df6770c62e56aad4
Harry-Bro <likeharrybro@gmail.com>
2022-01-12 00:59:13 +0900

    :wrench: 로컬 환경 구성 (#3)
    + 스프링 시큐리티 h2 데이터베이스 접근 권한 해제

---
+++ backend/src/main/java/com/oclothes/global/config/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.oclothes.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("h2-console/**")
+                .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
+}
