a0bd8bdbae21a796ab71342bfe65d77a2192d714
Harry-Bro <likeharrybro@gmail.com>
2022-01-11 18:14:52 +0900

    :tada: 백앤드 프로젝트 생성 (#3)

---
+++ backend/src/main/java/com/oclothes/OclothesApplication.java
@@ -0,0 +1,13 @@
+package com.oclothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OclothesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OclothesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/oclothes/OclothesApplicationTests.java
@@ -0,0 +1,5 @@
+package com.oclothes;
+
+class OclothesApplicationTests {
+
+}
