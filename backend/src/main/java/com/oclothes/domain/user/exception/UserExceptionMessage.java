ebb9e6242072156efb60f0edceeaf91ccb5b9e3e
Harry-Bro <likeharrybro@gmail.com>
2022-01-13 20:18:29 +0900

    :sparkles: 로고 추가
    + 유저 엑셉션 메시지 맞춤법 수정

--- backend/src/main/java/com/oclothes/domain/user/exception/UserExceptionMessage.java
+++ backend/src/main/java/com/oclothes/domain/user/exception/UserExceptionMessage.java
@@ -9,7 +9,7 @@ public enum UserExceptionMessage {
 
     ALREADY_EXISTS_EMAIL("해당 이메일은 이미 회원가입된 이메일입니다."),
     EMAIL_AUTHENTICATION_CODE_TOO_MANY_REQUEST("이메일 인증 요청 3분 후 재시도해주세요."),
-    USER_STATUS_IS_WAIT("해당 이메로 인증 번호가 전송된 상태입니다. 이메일 인증을 완료해주세요."),
+    USER_STATUS_IS_WAIT("해당 이메일로 인증 번호가 전송된 상태입니다. 이메일 인증을 완료해주세요."),
     USER_NOT_FOUND("해당 유저가 존재하지 않습니다."),
     USER_STATUS_IS_ALREADY_NORMAL("해당 이메일은 이미 인증이 완료 되었습니다."),
     EMAIL_AUTHENTICATION_CODE_NOT_FOUND("이메일 인증 요청을 해주세요."),
