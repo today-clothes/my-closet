efd043aee6e40636cfbc7f27767fb37d30a61726
Harry-Bro <likeharrybro@gmail.com>
2022-01-30 15:36:01 +0900

    :recycle: Swagger 설정 변경 (#47)

--- backend/src/main/java/com/oclothes/global/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/com/oclothes/global/config/swagger/SwaggerConfig.java
@@ -1,22 +1,17 @@
-package com.oclothes.global.config.api;
+package com.oclothes.global.config.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration {
+public class SwaggerConfig {
 
     private static final String API_NAME = "오늘의 옷 API";
     private static final String API_VERSION = "0.0.1";
@@ -24,8 +19,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket apiV1() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("groupName1")
+        return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.oclothes.domain"))
                 .paths(PathSelectors.any())
