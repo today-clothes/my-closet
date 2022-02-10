830f2335fef40d4a7b37aa9dfbb6a4b4129eb7ed
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2022-02-10 23:35:35 +0900

    :sparkles: 옷 상세정보 조회 기능 추가 (#53) (#92)

--- backend/src/main/java/com/oclothes/global/config/environment/LocalAndTestEnvironmentInitDataConfig.java
+++ backend/src/main/java/com/oclothes/global/config/environment/LocalAndTestEnvironmentInitDataConfig.java
@@ -39,7 +39,9 @@ public class LocalAndTestEnvironmentInitDataConfig implements ApplicationRunner,
     public void run(ApplicationArguments args) throws Exception {
         this.userRepository.save(this.createUser("admin@test.com", User.Role.ROLE_ADMIN));
         final User user = this.userRepository.save(this.createUser("user@test.com", User.Role.ROLE_USER));
+        final User user1 = this.userRepository.save(this.createUser("james@test.com", User.Role.ROLE_USER));
         final Closet c1 = this.closetRepository.save(new Closet("c1",  user));
+        final Closet c2 = this.closetRepository.save(new Closet("c2",  user1));
 
         this.seasonTagRepository.saveAll(List.of(
                 new SeasonTag("봄"),
@@ -69,6 +71,9 @@ public class LocalAndTestEnvironmentInitDataConfig implements ApplicationRunner,
                 .role(role)
                 .email(new Email(email))
                 .password(this.passwordEncoder.encode("123456"))
+                .nickname("james")
+                .height(160)
+                .weight(50)
                 .build();
     }
 
