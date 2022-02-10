80d1c977004dc0973e529f1d3dbb90d3011d56c6
Jae <gjwodud119@gmail.com>
2022-02-10 18:29:41 +0900

    :lipstick: 스타일 편집 UI 추가 (#31)

--- android/app/src/main/java/com/oclothes/mycloset/ui/main/closet/DetailFragment.kt
+++ android/app/src/main/java/com/oclothes/mycloset/ui/main/closet/DetailFragment.kt
@@ -5,12 +5,25 @@ import com.oclothes.mycloset.ui.BaseFragment
 
 class DetailFragment(val f : MainFragment) : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
 
+
     override fun initAfterBinding() {
         binding.detailMainFrmSpl.anchorPoint = 0.5f
         binding.detailMainFrmSpl.isClipPanel = false
         binding.detailMainBackBtnIv.setOnClickListener{
             f.getBinding().mainFragmentVp.currentItem = 1
         }
+
+//        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()
+//        ) { result : ActivityResult ->
+//            binding.detailMainImageIv.setImageURI(result.data?.data)
+//        }
+//
+//        binding.detailMainImageIv.setOnClickListener {
+//            val intent = Intent(Intent.ACTION_PICK);
+//            intent.type = MediaStore.Images.Media.CONTENT_TYPE
+//            intent.type = "image/*"
+//            getContent.launch(intent)
+//        }
     }
 
 //    {
