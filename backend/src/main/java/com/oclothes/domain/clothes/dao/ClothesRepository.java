66c15a26e796d056c5ed8be02142553ffe0d895a
Harry-Bro <likeharrybro@gmail.com>
2022-02-12 02:30:20 +0900

    :sparkles: 옷 다중 삭제 기능 추가 (#97)

--- backend/src/main/java/com/oclothes/domain/clothes/dao/ClothesRepository.java
+++ backend/src/main/java/com/oclothes/domain/clothes/dao/ClothesRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long>, ClothesSupportRepository {
@@ -20,4 +22,8 @@ public interface ClothesRepository extends JpaRepository<Clothes, Long>, Clothes
     Optional<Clothes> findByIdAndUser(Long id, @NonNull User user);
 
     long countByCloset(@NonNull Closet closet);
+
+    void deleteAllByIdIn(Collection<Long> ids);
+
+    List<Clothes> findAllByIdIn(Collection<Long> ids);
 }
--- backend/src/main/java/com/oclothes/infra/file/FileService.java
+++ backend/src/main/java/com/oclothes/infra/file/FileService.java
@@ -2,10 +2,14 @@ package com.oclothes.infra.file;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface FileService {
     String saveImage(MultipartFile file);
 
     byte[] getImage(String key);
 
     void delete(String key);
+
+    void deleteAll(List<String> keys);
 }
